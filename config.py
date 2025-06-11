911f65a9109282e1274480944aaf36ab2c158858
Incredaimaso <159280005+Incredaimaso@users.noreply.github.com>
2025-06-11 21:06:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,17 +3,17 @@
 #..........Just one requests do not remove my credit..........#
 
 import os
-API_ID = 1234567
-API_HASH = ""
-BOT_TOKEN = ""
+API_ID = 22403100
+API_HASH = "ccbc3f662735abfa604ef6309ba76e67"
+BOT_TOKEN = "7070324312:AAFObFG5cnn0zs5oCRYN4fRFQMX3LICVM8w"
 #Logs
-LOG_CHANNEL = "" 
+LOG_CHANNEL = "-1002432275758" 
 # MongoDB setup
 MONGO_URL = ""
-DB_NAME = ""
+DB_NAME = "mongodb+srv://sumitsajwan135:<gameno01>@cluster0.ja0i0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 DOWNLOAD_DIR = "./downloads"
 #ADMIN INFO
-ADMIN = 
+ADMIN = 7950514048
 
 START_PIC = os.environ.get("START_PIC", "https://graph.org/file/dfd1842d8a2dcc536a2b7.jpg https://graph.org/file/b55f0baaa7a6fde7c5682.jpg https://graph.org/file/3298ca8910c82f33418a8.jpg https://graph.org/file/4e4935469a7214c734721.jpg https://graph.org/file/fa5f2b241fe77beff8ba0.jpg https://graph.org/file/bcb9969f78ab4a47a483c.jpg https://graph.org/file/a9af161696d82f17b8888.jpg https://graph.org/file/d23b00650c00ce4d9a467.jpg https://graph.org/file/4dc0b3dfaad61fbcf0a49.jpg https://graph.org/file/7088315e9b0b6a2fa7118.jpg https://graph.org/file/9c9911d06e5f2316febb9.jpg https://graph.org/file/d2ee185180469cfd28071.jpg https://graph.org/file/dea04b2d615406aeb0181.jpg https://graph.org/file/98b63d3bb84984a68cc76.jpg").split()
